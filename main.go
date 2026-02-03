99ebc855cd2f8cddbebc87fb4d8506a19f72d4bf
boicotaz <tolisgerodimos94@gmail.com>
2026-02-03 16:45:06 +0200

    Add go subroutine to handle logging

--- main.go
+++ main.go
@@ -22,15 +22,22 @@ type Task struct {
 	Status      string `json:"status"`
 }
 
-func tasksHandler(queue chan Task) http.HandlerFunc {
+type TaskEvent struct {
+	ID        int       `json:"id,omitempty"`
+	Action    string    `json:"action,omitempty"`
+	Timestamp time.Time `json:"ts"`
+	Level     string    `json:"level"`
+}
+
+func tasksHandler(taskQueue chan Task, logQueue chan TaskEvent) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         switch r.Method {
         case http.MethodGet:
             handleGetTasks(w, r)
         case http.MethodPut:
-            handleUpdateTask(w, r)
+            handleUpdateTask(w, r, logQueue)
         case http.MethodPost:
-            handleCreateTask(w, r, queue)
+            handleCreateTask(w, r, taskQueue, logQueue)
         default:
             http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
         }
@@ -59,7 +66,7 @@ func handleGetTasks(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
 }
 
-func handleUpdateTask(w http.ResponseWriter, r *http.Request) {
+func handleUpdateTask(w http.ResponseWriter, r *http.Request, logQueue chan TaskEvent) {
 	var t Task
 
 	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
@@ -68,6 +75,11 @@ func handleUpdateTask(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if t.ID == 0 {
+		logQueue <- TaskEvent{
+			Action:    "update_failed_no_id",
+			Level:     "error",
+			Timestamp: time.Now(),
+		}
 		http.Error(w, "task id is required", http.StatusBadRequest)
 		return
 	}
@@ -91,12 +103,19 @@ func handleUpdateTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	logQueue <- TaskEvent{
+		ID:        t.ID,
+		Action:    "updated",
+		Level:     "info",
+		Timestamp: time.Now(),
+	}
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("task updated"))
 }
 
 
-func handleCreateTask(w http.ResponseWriter, r *http.Request, taskQueue chan Task) {
+func handleCreateTask(w http.ResponseWriter, r *http.Request, taskQueue chan Task, logQueue chan TaskEvent) {
 	var t Task
 
 	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
@@ -129,6 +148,14 @@ func handleCreateTask(w http.ResponseWriter, r *http.Request, taskQueue chan Tas
 	// 👇 async work
 	taskQueue <- t
 
+	// Log the creation event
+	logQueue <- TaskEvent{
+		ID:        t.ID,
+		Action:    "created",
+		Level:     "info",
+		Timestamp: time.Now(),
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(t)
@@ -144,31 +171,85 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ready"))
 }
 
-func taskWorker(taskQueue chan Task) {
-	log.Println("Task worker started")
+func taskWorker(taskQueue chan Task, logQueue chan TaskEvent) {
+	startup_message := TaskEvent{
+		Action:    "task_worker_started",
+		Level:     "info",
+		Timestamp: time.Now(),
+	}
+	logQueue <- startup_message
 
 	for task := range taskQueue {
-		log.Println("Processing task:", task.ID)
-
 		// mark as processing
+		logQueue <- TaskEvent{
+			ID:    task.ID,
+			Action:    "processing_started",
+			Level:     "info",
+			Timestamp: time.Now(),
+		}
+
 		if _, err := db.Exec("UPDATE tasks SET status = $1 WHERE id = $2", "processing", task.ID); err != nil {
-			log.Println("Failed to update task status:", err)
+			logQueue <- TaskEvent{
+				ID:    task.ID,
+				Action:    "processing_failed",
+				Level:     "error",
+				Timestamp: time.Now(),
+			}
 			continue
 		}
 
 		time.Sleep(5 * time.Second) // Simulate work
 
 		if _, err := db.Exec("UPDATE tasks SET status = $1 WHERE id = $2", "done", task.ID); err != nil {
-			log.Println("Failed to update task status:", err)
+			logQueue <- TaskEvent{
+				ID:    task.ID,
+				Action:    "completion_failed",
+				Level:     "error",
+				Timestamp: time.Now(),
+			}
 			continue
 		}
 
-		log.Println("Completed task:", task.ID)
+		logQueue <- TaskEvent{
+			ID:    task.ID,
+			Action:    "completed",
+			Level:     "info",
+			Timestamp: time.Now(),
+		}
+
+	}
+}
+
+func logWorker(logQueue chan TaskEvent) {
+
+	startup_message, _ := json.Marshal(TaskEvent{
+		Action:    "log_worker_started",
+		Level:     "info",
+		Timestamp: time.Now(),
+	})
+	log.Println(string(startup_message))
+
+	for event := range logQueue {
+		//log.Printf("Task %d %s at %s\n", event.TaskID, event.Action, event.Timestamp.Format(time.RFC3339))
+		message, err := json.Marshal(event)
+		if err != nil {
+			log.Println("Failed to marshal log event:", err)
+			continue
+		}
+		log.Println(string(message))
 	}
 }
 
 func main() {
+	// Disable timestamp in default logger for cleaner JSON logs
+	log.SetFlags(0)
 
+	// --- Start Log Worker ---
+	var logQueue chan TaskEvent
+	logQueue = make(chan TaskEvent, 100)
+
+	go logWorker(logQueue)
+	
 	// DB connection values from env (K8s ConfigMap/Secret friendly)
 	dbHost := os.Getenv("DB_HOST")
 	dbUser := os.Getenv("DB_USER")
@@ -188,17 +269,21 @@ func main() {
 	if db_err := db.Ping(); db_err != nil {
 		log.Fatal("DB ping failed:", db_err)
 	}
-	log.Println("✅ Connected to Postgres")
+	logQueue <- TaskEvent{
+		Action:    "db_connected",
+		Level:     "info",
+		Timestamp: time.Now(),
+	}
 
 	// --- Start Task Worker ---
 	var taskQueue chan Task
 	taskQueue = make(chan Task, 100)
 
-	go taskWorker(taskQueue)
+	go taskWorker(taskQueue, logQueue)
 
 	http.HandleFunc("/ready", readinessHandler)
 
-	http.HandleFunc("/tasks", tasksHandler(taskQueue))
+	http.HandleFunc("/tasks", tasksHandler(taskQueue, logQueue))
 
 	// Read PORT from environment variable, default to 8080
 	port := os.Getenv("PORT")
@@ -206,7 +291,11 @@ func main() {
 		port = "8080"
 	}
 
-	log.Printf("Server starting on port %s...\n", port)
+	logQueue <- TaskEvent{
+		Action:    "server_starting",
+		Level:     "info",
+		Timestamp: time.Now(),
+	}
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
